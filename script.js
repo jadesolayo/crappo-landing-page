19a99c1156064921444d74fc0940b8415c393004
jadesolayo <oloyedeyetundejadesola@gmail.com>
2022-10-08 18:54:03 +0100

    modified index.html and style.css

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function leftToggler() {
 
-    if (document.getElementById('nav').style.left == '0px') {
-        document.getElementById('nav').style.left = '100%'
-    }
-    else {
-    document.getElementById('nav').style.left = '0'
-    }
+function leftToggler() {
+    console.log(document.getElementById("nav"));
+    alert("bastard")
+    document.getElementById("nav").style.left = "0%";
+
+}
+
+function closeToggler() {
+    console.log(document.getElementById("nav"));
+    document.getElementById("nav").style.left = "100%"; 
 }
